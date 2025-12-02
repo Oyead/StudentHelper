5a424196ecc276b8b8414fb5852cbf02aefa7063
Eyad <eyadswailam12345@gmail.com>
2025-12-02 22:04:34 +0200

    add basic select file func

--- utils.py
+++ utils.py
@@ -1,2 +1,13 @@
-def greet_user(name):
-    return f"Hello, {name}!"
+import os
+import tkinter as tk
+from tkinter import filedialog
+def select_file():
+    file_path = filedialog.askopenfilename(title="Select a file",
+    filetypes=(
+        ("All files", "*.*"),
+    ))
+    print("Selected file:", file_path)
+    fileType=os.path.splitext(file_path)[1][1:]
+    fileName=os.path.basename(file_path)
+    print("File Type:" ,fileType , "file")
+    print("File Name:",fileName)
