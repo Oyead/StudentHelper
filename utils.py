b1f7e4a1d9fd6ba78d4f1784d4fc43fd5c02ec85
Eyad <eyadswailam12345@gmail.com>
2025-12-09 19:30:25 +0200

    edits utils

--- utils.py
+++ utils.py
@@ -1,13 +1,18 @@
 import os
-import tkinter as tk
-from tkinter import filedialog
+from PyQt5.QtWidgets import QFileDialog
+
 def select_file():
-    file_path = filedialog.askopenfilename(title="Select a file",
-    filetypes=(
-        ("All files", "*.*"),
-    ))
-    print("Selected file:", file_path)
-    fileType=os.path.splitext(file_path)[1][1:]
-    fileName=os.path.basename(file_path)
-    print("File Type:" ,fileType , "file")
-    print("File Name:",fileName)
+    file_path, _ = QFileDialog.getOpenFileName(
+        None,
+        "Select a file",
+        "",
+        "All files (*.*)"
+    )
+    if file_path:
+        print("Selected file:", file_path)
+        fileType = os.path.splitext(file_path)[1][1:]
+        fileName = os.path.basename(file_path)
+        print("File Type:", fileType)
+        print("File Name:", fileName)
+        return file_path
+    return None
